7a14d0687c75a6af35948d2a68397c1462291b9f
jpthiery <jpthiery@xebia.fr>
2018-04-07 15:59:16 +0200

    Adding batch mode

--- jobs/src/main/groovy/kodokojoPropertyconfigurer.groovy
+++ jobs/src/main/groovy/kodokojoPropertyconfigurer.groovy
@@ -12,7 +12,7 @@ job('property-configurer') {
   }
   label('jkmaster')
   steps {
-    shell 'docker run --rm -v /home/jpthiery/worskapce/xke/jenkins-job-dsl-sample/jenkins/workspace/property-configurer:/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-9-slim /bin/bash -c "mvn clean verify"'
+    shell 'docker run --rm -v /home/jpthiery/worskapce/xke/jenkins-job-dsl-sample/jenkins/workspace/property-configurer:/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-9-slim /bin/bash -c "mvn -B clean verify"'
   }
   logRotator(15, 10)
 }
