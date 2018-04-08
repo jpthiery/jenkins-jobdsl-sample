74fa36783d5fcd091547bb59aced8940d95bfe61
jpthiery <jpthiery@xebia.fr>
2018-04-08 12:52:49 +0200

    Fixing path in docker copy

--- src/main/groovy/fr/xebia/jpthiery/jenkins/jobs/MavenJobBuilder.groovy
+++ src/main/groovy/fr/xebia/jpthiery/jenkins/jobs/MavenJobBuilder.groovy
@@ -12,7 +12,7 @@ class MavenJobBuilder<T extends MavenJobBuilder>  extends JobBuilder<T>  {
         ctx.shell """
                   | rm -f target || true
                   | containerId=\$(docker create --rm -w /usr/src/mymaven maven:3-jdk-9-slim /bin/bash -c "${mvnCmd}")
-                  | docker cp /var/jenkins_home/workspace/${jobName} \${containerId}:/usr/src/mymaven
+                  | docker cp /var/jenkins_home/workspace/${jobName}/ \${containerId}:/usr/src/mymaven/
                   | docker start -a \${containerId}
                   | mkdir target
                   | docker cp \${containerId}:/usr/src/mymaven/target/ target/
