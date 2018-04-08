00e1d341c5187d232bfa2c1ddf261d1205373e45
jpthiery <jpthiery@xebia.fr>
2018-04-08 13:54:46 +0200

    :Fixing docker cp path

--- src/main/groovy/fr/xebia/jpthiery/jenkins/jobs/MavenJobBuilder.groovy
+++ src/main/groovy/fr/xebia/jpthiery/jenkins/jobs/MavenJobBuilder.groovy
@@ -10,13 +10,13 @@ class MavenJobBuilder<T extends MavenJobBuilder>  extends JobBuilder<T>  {
     addStep {
       ctx ->
         ctx.shell """
-                  | rm -f target || true
-                  | containerId=\$(docker create --rm -w /usr/src/mymaven maven:3-jdk-9-slim /bin/bash -c "${mvnCmd}")
-                  | docker cp /var/jenkins_home/workspace/${jobName}/ \${containerId}:/usr/src/mymaven/
-                  | docker start -a \${containerId}
-                  | mkdir target
-                  | docker cp \${containerId}:/usr/src/mymaven/target/ target/
-                  | chmod -R 777 target
+                  |rm -f target || true
+                  |mkdir target
+                  |chmod -R 777 target
+                  |containerId=\$(docker create -w /usr/src/mymaven maven:3-jdk-9-slim /bin/bash -c "${mvnCmd}")
+                  |docker cp /var/jenkins_home/workspace/${jobName}/. \${containerId}:/usr/src/mymaven/
+                  |docker start -a \${containerId}
+                  |docker cp \${containerId}:/usr/src/mymaven/target/. target
                   """.stripMargin()
     }
     this
