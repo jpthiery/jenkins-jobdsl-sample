0cc73f7d865fe890975e4f8a240e2322297b9746
jpthiery <jpthiery@xebia.fr>
2018-04-08 15:39:06 +0200

    Defined severals jobs

--- jobs/src/main/groovy/kodokojoPropertyconfigurer.groovy
+++ jobs/src/main/groovy/kodokojoPropertyconfigurer.groovy
@@ -2,11 +2,43 @@ import fr.xebia.jpthiery.jenkins.jobs.MavenJobBuilder
 
 def dslFactory = this
 
-def jobBuilder = new MavenJobBuilder("property-configurer")
-jobBuilder.repositoryUrl('https://github.com/kodokojo/property-configurer.git')
-  .labels('jkmaster')
+def repository = 'kodokojo'
+def projects = ['property-configurer']
 
-jobBuilder.lookupGithubBranch('kodokojo', 'property-configurer')
-jobBuilder.mvnCmd("mvn -B clean verify")
+def environments = [
+  'dev1',
+  'dev2',
+  'dev3',
+  'int1',
+  'int2',
+  'int3',
+  'uat1',
+  'uat2',
+  'uat3',
+  'recette',
+  'prodlike1',
+  'prodlike2',
+  'prodlike3',
+  'production'
+]
 
-jobBuilder.build(dslFactory)
+environments.each { env ->
+  folder(env)
+  projects.each { project ->
+
+    def jobBuilder = new MavenJobBuilder("${env}/${project}")
+    jobBuilder.repositoryUrl("https://github.com/${repository}/${project}.git")
+      .labels('jkmaster')
+
+    jobBuilder.lookupGithubBranch(repository, project)
+    jobBuilder.mvnCmd("mvn -B clean verify")
+    jobBuilder.logRotator {
+      ctx ->
+        ctx.daysToKeep(15)
+        ctx.numToKeep(10)
+    }
+
+    jobBuilder.build(dslFactory)
+  }
+}
+
