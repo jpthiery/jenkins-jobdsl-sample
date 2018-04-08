12550c8d4793963febcbbcd44bf3e086ee6724e9
jpthiery <jpthiery@xebia.fr>
2018-04-08 12:50:42 +0200

    Fix bad docker start cmd

--- src/main/groovy/fr/xebia/jpthiery/jenkins/jobs/MavenJobBuilder.groovy
+++ src/main/groovy/fr/xebia/jpthiery/jenkins/jobs/MavenJobBuilder.groovy
@@ -13,9 +13,10 @@ class MavenJobBuilder<T extends MavenJobBuilder>  extends JobBuilder<T>  {
                   | rm -f target || true
                   | containerId=\$(docker create --rm -w /usr/src/mymaven maven:3-jdk-9-slim /bin/bash -c "${mvnCmd}")
                   | docker cp /var/jenkins_home/workspace/${jobName} \${containerId}:/usr/src/mymaven
-                  | docker start -a
+                  | docker start -a \${containerId}
                   | mkdir target
                   | docker cp \${containerId}:/usr/src/mymaven/target/ target/
+                  | chmod -R 777 target
                   """.stripMargin()
     }
     this
