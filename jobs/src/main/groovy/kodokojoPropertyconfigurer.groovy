332b95cf99b701d83e71c45d4976bcd944a6e6d6
jpthiery <jpthiery@xebia.fr>
2018-04-08 12:25:35 +0200

    Use DockerMvnJobBuilder

--- jobs/src/main/groovy/kodokojoPropertyconfigurer.groovy
+++ jobs/src/main/groovy/kodokojoPropertyconfigurer.groovy
@@ -1,13 +1,11 @@
-import fr.xebia.jpthiery.jenkins.jobs.JobBuilder
+import fr.xebia.jpthiery.jenkins.jobs.DockerMavenJobBuilder
 
 def dslFactory = this
 
-def jobBuilder = new JobBuilder("property-configurer")
+def jobBuilder = new DockerMavenJobBuilder("property-configurer")
 jobBuilder.repositoryUrl('https://github.com/kodokojo/property-configurer.git')
   .labels('jkmaster')
-  .addStep {
-  ctx ->
-    ctx.shell 'docker run --rm -v /home/jpthiery/worskapce/xke/jenkins-job-dsl-sample/jenkins/workspace/property-configurer:/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-9-slim /bin/bash -c "mvn -B clean verify"'
-}
+
+jobBuilder.mvnCmd("mvn -B clean verify")
 
 jobBuilder.build(dslFactory)
